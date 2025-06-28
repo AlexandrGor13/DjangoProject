e9f48a184d001fa6d79efa8854195c1ff94877e4
alexandr <gorbenich@gmail.com>
2025-06-28 19:58:01 +0300

    create payment

--- app_orders/payment.py
+++ app_orders/payment.py
@@ -1,6 +1,8 @@
 import json
 
 from django.conf import settings
+from django.utils.timezone import now
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from yookassa import Configuration, Payment
 
@@ -22,33 +24,31 @@ def create_order_payment(request):
             amount=order.total_amount,
             transaction_id=transaction_id,
         )
-        payment_data = create_payment(payment)
+        payment_data = create_payment(request, payment)
 
         if payment_data:
-            payment_id = payment_data.get('id')
-            payment_url = (payment_data.get('confirmation')).get('confirmation_url')
+            payment_id = payment_data.get("id")
+            payment_url = (payment_data.get("confirmation")).get("confirmation_url")
             payment.transaction_id = payment_id
             payment.save()
     return payment_url
 
 
-def create_payment(payment):
+def create_payment(request, payment):
     try:
-        if payment.status != 'pending':
+        if payment.status != "pending":
             return None
         payment_data = {
-            'amount': {
-                'value': str(payment.amount),
-                'currency': 'RUB'},
-            'confirmation': {
-                'type': 'redirect',
-                'return_url': f'http://127.0.0.1:8000/order/payment/status/?id={payment.id}'
+            "amount": {"value": str(payment.amount), "currency": "RUB"},
+            "confirmation": {
+                "type": "redirect",
+                "return_url": f"http://{request.get_host()}/order/payment/status/?id={payment.id}",
             },
-            'capture': True,
-            'description': f'Оплата заказа № {payment.order.id}',
-            'metadata': {
-                'order_id': payment.order.id,
-            }
+            "capture": True,
+            "description": f"Оплата заказа № {payment.order.id}",
+            "metadata": {
+                "order_id": payment.order.id,
+            },
         }
         payment = Payment.create(payment_data)
         payment_data = json.loads(payment.json())
@@ -62,12 +62,22 @@ def get_status_payment(payment_m: PaymentModel):
     try:
         payment_id = payment_m.transaction_id
         payment = Payment.find_one(payment_id=payment_id)
-        if payment.status == 'succeeded':
-            status = 'paid'
-        elif payment.status == 'canceled':
-            status = 'canceled'
+        if payment.status == "succeeded":
+            status = "paid"
+        elif payment.status == "canceled":
+            status = "canceled"
         else:
-            status = 'pending'
+            status = "pending"
+            try:
+                PeriodicTask.objects.create(
+                    name="Repeat order {}".format(payment_m.order.id),
+                    task="repeat_payment_make",
+                    interval=IntervalSchedule.objects.get(every=10, period="seconds"),
+                    args=json.dumps([payment_m.id, payment_id]),
+                    start_time=now(),
+                )
+            except Exception as e:
+                print(e)
         payment_m.status = status
         payment_m.save()
         return status
---
+++ app_orders/tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from django_celery_beat.models import PeriodicTask
+from yookassa import Payment
+
+from .models import Payment as PaymentModel
+
+
+@shared_task(name="repeat_payment_make")
+def repeat_order_make(payment_id, transaction_id):
+    payment = Payment.find_one(payment_id=transaction_id)
+    if payment.status == "succeeded":
+        payment_m = PaymentModel.objects.get(id=payment_id)
+        payment_m.status = "paid"
+        payment_m.save()
+        task = PeriodicTask.objects.get(
+            name="Repeat order {}".format(payment_m.order.id)
+        )
+        task.enabled = False
+        task.save()
+    else:
+        # Необходимая логика при повторной отправке заказа
+        print("Я должна повторно оформлять заказ каждые 10 минут")
